2ff439895b81fb399337248cfc7bef88b8064ac9
Samy <s_rahim@estin.dz>
2023-08-30 12:38:37 +0100

    added both host paths

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-export const BACKEND_DOMAIN = "https://bytecraft-ranking-api.onrender.com/api";
+// export const BACKEND_DOMAIN = "https://bytecraft-ranking-api.onrender.com/api";
+export const BACKEND_DOMAIN = "http://127.0.0.1:9000/api";
