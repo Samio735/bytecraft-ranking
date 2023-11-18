8085d287256ff8925b6cf52bd57e037883eedd91
Samy <s_rahim@estin.dz>
2023-11-18 17:30:33 +0100

    https

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const BACKEND_DOMAIN = "http://165.232.126.126/api";
+export const BACKEND_DOMAIN = "https://bcranking.samyrahim.me/api";
 // "https://ranking-api-gsdqe.ondigitalocean.app/api";
 // export const BACKEND_DOMAIN = "http://127.0.0.1:9000/api";
 export const LOGIN_KEY = "/login/";
